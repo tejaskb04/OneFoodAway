1509cbfde29cbb0ee5e11b54082e57c6b4430d35
tejaskb04 <tejaskb@gmail.com>
2017-09-21 00:07:28 -0700

    Added Code

---
+++ OneFoodAway/app/src/main/java/com/tejas_bharadwaj/onefoodaway/MapActivity.java
@@ -0,0 +1,274 @@
+package com.tejas_bharadwaj.onefoodaway;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private final String GOOGLE_PLACES_API_KEY = "AIzaSyAH008n41rXGsO2oYtJgZduebNYwN127_I";
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100;
+    private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private HashMap<String, String> placeTitleId = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapActivity.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMapToolbarEnabled(false);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else {
+            map.setMyLocationEnabled(true);
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(MapActivity.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            CameraPosition cameraPosition = new CameraPosition.Builder()
+                                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                    .zoom(12)
+                                    .bearing(270)
+                                    .tilt(20)
+                                    .build();
+                            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                            String placeSearchUrl = createPlaceSearchUrl(location.getLatitude(), location.getLongitude());
+                            new PlaceSearchCallBackTask().execute(placeSearchUrl);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        map.setMyLocationEnabled(true);
+                        fusedLocationProviderClient.getLastLocation()
+                                .addOnSuccessListener(MapActivity.this, new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                                                .zoom(12)
+                                                .bearing(270)
+                                                .tilt(20)
+                                                .build();
+                                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                                        String placeSearchUrl = createPlaceSearchUrl(location.getLatitude(), location.getLongitude());
+                                        new PlaceSearchCallBackTask().execute(placeSearchUrl);
+                                    }
+                                });
+                    } catch (SecurityException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    // STUB
+                }
+                return;
+            }
+        }
+    }
+
+    private String createPlaceSearchUrl(double lat, double lng) {
+        StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        stringBuilder.append("location=").append(lat).append(",").append(lng);
+        stringBuilder.append("&radius=").append(5000);
+        stringBuilder.append("&types=").append("cafe|meal_delivery|meal_takeaway|restaurant");
+        stringBuilder.append("&sensor=true");
+        stringBuilder.append("&key=").append(GOOGLE_PLACES_API_KEY);
+        return stringBuilder.toString();
+    }
+
+    private String createPlaceDetailsUrl(String placeId) {
+        StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/details/json?");
+        stringBuilder.append("placeid=").append(placeId);
+        stringBuilder.append("&key=").append(GOOGLE_PLACES_API_KEY);
+        return stringBuilder.toString();
+    }
+
+    private class PlaceSearchCallBackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection
+                        .getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Show Error Message
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String name = "NA";
+            String snippet = "NA";
+            String placeId;
+            double lat, lng;
+            try {
+                JSONObject data = new JSONObject(result);
+                if (data.getString("status").equalsIgnoreCase("OK")) {
+                    JSONArray jsonArray = data.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        final JSONObject place = jsonArray.getJSONObject(i);
+                        if (!place.isNull("name")) {
+                            name = place.getString("name");
+                        }
+                        if (!place.isNull("rating") && !place.isNull("price_level")) {
+                            snippet = "Rating: " + place.getString("rating") + " " + "Price Level: "
+                                    + place.getString("price_level");
+                        }
+                        if (!place.isNull("place_id")) {
+                            placeId = place.getString("place_id");
+                            placeTitleId.put(name, placeId);
+                        }
+                        lat = place.getJSONObject("geometry").getJSONObject("location")
+                                .getDouble("lat");
+                        lng = place.getJSONObject("geometry").getJSONObject("location")
+                                .getDouble("lng");
+                        map.addMarker(new MarkerOptions()
+                                .position(new LatLng(lat, lng))
+                                .title(name)
+                                .snippet(snippet));
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                // Show Error Message
+            }
+            map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    String name = null;
+                    String snippet = null;
+                    if (marker.getTitle() != null) {
+                        name = marker.getTitle();
+                    }
+                    if (marker.getSnippet() != null) {
+                        snippet = marker.getSnippet();
+                    }
+                    String placeDetailsUrl = createPlaceDetailsUrl(placeTitleId.get(marker.getTitle()));
+                    new PlaceDetailsCallBackTask().execute(placeDetailsUrl);
+                    Intent intent = new Intent(MapActivity.this, MarkerActivity.class);
+                    intent.putExtra("title", name);
+                    intent.putExtra("snippet", snippet);
+                    startActivity(intent);
+                    return false;
+                }
+            });
+        }
+    }
+
+    private class PlaceDetailsCallBackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection
+                        .getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Show Error Message
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String phoneNumber = "NA";
+            String photoStrings = "";
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                if (jsonObject.getString("status").equalsIgnoreCase("OK")) {
+                    JSONObject data = jsonObject.getJSONObject("result");
+                    if (!data.isNull("formatted_phone_number")) {
+                        phoneNumber = data.getString("formatted_phone_number");
+                    }
+                    JSONArray photoArray = data.getJSONArray("photos");
+                    for (int i = 0; i < photoArray.length(); i++) {
+                        JSONObject pictureObject = photoArray.getJSONObject(i);
+                        if (!pictureObject.isNull("photo_reference")) {
+                            photoStrings += pictureObject.getString("photo_reference");
+                            photoStrings += " ";
+                        }
+                    }
+                }
+                photoStrings = photoStrings.trim();
+                Intent intent = new Intent("photoIntent");
+                intent.putExtra("phoneNumber", phoneNumber);
+                intent.putExtra("photoStrings", photoStrings);
+                LocalBroadcastManager.getInstance(MapActivity.this).sendBroadcast(intent);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                // Show Error Message
+            }
+        }
+    }
+}
---
+++ OneFoodAway/app/src/main/java/com/tejas_bharadwaj/onefoodaway/MarkerActivity.java
@@ -0,0 +1,237 @@
+package com.tejas_bharadwaj.onefoodaway;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.KeyEvent;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MarkerActivity extends AppCompatActivity {
+
+    private final String GOOGLE_PLACES_API_KEY = "AIzaSyAH008n41rXGsO2oYtJgZduebNYwN127_I";
+    private TextView textViewTitle;
+    private TextView textViewRating;
+    private RatingBar ratingBar;
+    private TextView textViewPriceLevel;
+    private TextView textViewPhoneNumber;
+    private LinearLayout photoLayout;
+    private LinearLayout priceLevelLayout;
+    private int width = 0;
+    private int height = 0;
+    private String phoneNumber = null;
+    private String photoStrings = "";
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            phoneNumber = intent.getStringExtra("phoneNumber");
+            if (!phoneNumber.equals(null) || !phoneNumber.equals("NA")) {
+                textViewPhoneNumber.setText("Phone Number: " + phoneNumber);
+            }
+            photoStrings = intent.getStringExtra("photoStrings");
+            ArrayList<String> parsedPhotoStrings = parsePhotoStrings(photoStrings);
+            for (int i = 0; i < parsedPhotoStrings.size(); i++) {
+                String photoUrl = createPlacePhotosUrl(parsedPhotoStrings.get(i));
+                ImageView imageView = new ImageView(MarkerActivity.this);
+                photoLayout.addView(imageView);
+                Picasso.with(MarkerActivity.this).load(photoUrl)
+                        .networkPolicy(NetworkPolicy.NO_CACHE, NetworkPolicy.NO_STORE)
+                        .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                        .resize(width, 0)
+                        //.fit()
+                        //.centerCrop()
+                        .into(imageView);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker);
+        textViewTitle = (TextView) findViewById(R.id.title);
+        textViewRating = (TextView) findViewById(R.id.rating);
+        ratingBar = (RatingBar) findViewById(R.id.rating_bar);
+        textViewPriceLevel = (TextView) findViewById(R.id.price_level);
+        textViewPhoneNumber = (TextView) findViewById(R.id.phone_number);
+        photoLayout = (LinearLayout) findViewById(R.id.photoLayout);
+        photoLayout.setDrawingCacheEnabled(false);
+        priceLevelLayout = (LinearLayout) findViewById(R.id.price_level_layout);
+        String title = getIntent().getStringExtra("title");
+        String snippet = getIntent().getStringExtra("snippet");
+        LocalBroadcastManager.getInstance(MarkerActivity.this).registerReceiver(receiver,
+                new IntentFilter("photoIntent"));
+        if (!title.equals("NA")) {
+            textViewTitle.setText(title);
+        } else {
+            textViewTitle.setText("NA");
+        }
+        if (!snippet.equals("NA")) {
+            String[] vals = parseSnippet(snippet);
+            double[] dVals = getNums(vals);
+            textViewRating.setText(vals[0]);
+            ratingBar.setNumStars(5);
+            ratingBar.setRating((float) dVals[0]);
+            textViewPriceLevel.setText(vals[1]);
+            for (int i = 0; i < (int) dVals[1]; i++) {
+                ImageView moneyImage = new ImageView(MarkerActivity.this);
+                Drawable moneyDrawable = ContextCompat.getDrawable(MarkerActivity.this, R.drawable.ic_attach_money_black_48dp);
+                moneyImage.setImageDrawable(moneyDrawable);
+                priceLevelLayout.addView(moneyImage);
+            }
+        } else {
+            ratingBar.setNumStars(5);
+            ratingBar.setRating(0);
+            textViewRating.setText("NA");
+            textViewPriceLevel.setText("NA");
+        }
+    }
+
+    private String[] parseSnippet(String snippet) {
+        String[] vals = new String[2];
+        vals[0] = snippet.substring(0, snippet.indexOf("P") - 1);
+        vals[1] = snippet.substring(snippet.indexOf("P"), snippet.length());
+        return vals;
+    }
+
+    private double[] getNums(String[] vals) {
+        double[] dVals = new double[2];
+        for (int i = 0; i < vals.length; i++) {
+            Pattern regex = Pattern.compile("(\\d+(?:\\.\\d+)?)");
+            Matcher matcher = regex.matcher(vals[i]);
+            if (matcher.find()){
+                try {
+                    dVals[i] = Double.parseDouble(matcher.group(1));
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return dVals;
+    }
+
+    /*private Drawable resizeDrawable(Drawable image) {
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, 50, 50, false);
+        return new BitmapDrawable(getResources(), bitmapResized);
+    }*/
+
+    private String createPlacePhotosUrl(String photoReference) {
+        StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/photo?");
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        width = displayMetrics.widthPixels;
+        height = displayMetrics.heightPixels;
+        stringBuilder.append("maxwidth=").append(width);
+        stringBuilder.append("&photoreference=").append(photoReference);
+        stringBuilder.append("&key=").append(GOOGLE_PLACES_API_KEY);
+        return stringBuilder.toString();
+    }
+
+    private ArrayList<String> parsePhotoStrings(String photoStrings) {
+        ArrayList<String> parsedPhotoStrings = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < photoStrings.length(); i++) {
+            if (photoStrings.charAt(i) != ' ') {
+                temp += photoStrings.charAt(i);
+            } else {
+                parsedPhotoStrings.add(temp);
+                temp = "";
+            }
+        }
+        parsedPhotoStrings.add(temp);
+        return parsedPhotoStrings;
+    }
+
+    /*private class PlacePhotosCallBackTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public PlacePhotosCallBackTask(ImageView imageView) {
+            this.imageView = imageView;
+        }
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String url = params[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = new java.net.URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                // Show Error Message
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Show Error Message
+            }
+            return bitmap;
+        }
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            imageView.setImageBitmap(result);
+        }
+    }*/
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void deleteCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+        } catch (Exception e) {}
+    }
+
+    private boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+            return dir.delete();
+        } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
+    }
+
+    // DEBUG METHOD BELOW
+    private void printStringArrayList(ArrayList<String> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+}
